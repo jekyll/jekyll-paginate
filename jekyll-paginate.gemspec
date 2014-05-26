69a4e0cd899b92fc71461bb16ad0cf314bbc3df5
Parker Moore <parkrmoore@gmail.com>
2014-05-26 13:14:01 -0400

    Move things around.

--- jekyll-paginate.gemspec
+++ jekyll-paginate.gemspec
@@ -1,7 +1,7 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'jekyll/paginate/version'
+require 'jekyll-paginate/version'
 
 Gem::Specification.new do |spec|
   spec.name          = "jekyll-paginate"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,21 @@
+require File.expand_path("../lib/jekyll/paginate", File.dirname(__FILE__))
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
+
+  def build_site(config = {})
+    base = build_configs({
+      'source'      => source_dir,
+      'destination' => dest_dir,
+      'paginate'    => 1
+    })
+    site = Jekyll::Site.new(site_configuration(
+      {"paginate" => 1}.merge(config)
+    ))
+    site.process
+    site
+  end
 end
