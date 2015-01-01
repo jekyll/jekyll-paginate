70efd9695cb49157fbc74509d5eb2e48ee266252
Parker Moore <parkrmoore@gmail.com>
2015-01-01 17:23:21 -0500

    Throw an error if the pagination path doesn't contain :num.

--- lib/jekyll-paginate/pagination.rb
+++ lib/jekyll-paginate/pagination.rb
@@ -14,7 +14,7 @@ module Jekyll
       # Returns nothing.
       def generate(site)
         if Pager.pagination_enabled?(site)
-          if template = template_page(site)
+          if template = self.class.template_page(site)
             paginate(site, template)
           else
             Jekyll.logger.warn "Pagination:", "Pagination is enabled, but I couldn't find " +
@@ -38,8 +38,7 @@ module Jekyll
       #                   "previous_page" => <Number>,
       #                   "next_page" => <Number> }}
       def paginate(site, page)
-        all_posts = site.site_payload['site']['posts']
-        all_posts = all_posts.reject { |p| p['hidden'] }
+        all_posts = site.site_payload['site']['posts'].reject { |post| post['hidden'] }
         pages = Pager.calculate_pages(all_posts, site.config['paginate'].to_i)
         (1..pages).each do |num_page|
           pager = Pager.new(site, num_page, all_posts, pages)
@@ -61,7 +60,7 @@ module Jekyll
       #
       # Returns the url of the template page
       def self.first_page_url(site)
-        if page = Pagination.new.template_page(site)
+        if page = Pagination.template_page(site)
           page.url
         else
           nil
@@ -73,8 +72,8 @@ module Jekyll
       # site - the Jekyll::Site object
       #
       # Returns the Jekyll::Page which will act as the pager template
-      def template_page(site)
-        site.pages.dup.select do |page|
+      def self.template_page(site)
+        site.pages.select do |page|
           Pager.pagination_candidate?(site.config, page)
         end.sort do |one, two|
           two.path.size <=> one.path.size
