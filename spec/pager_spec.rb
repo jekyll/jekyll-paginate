9a161332f654fc348c4da402c329097639f3269f
Stargator <wildbug@linuxmail.org>
2015-11-13 22:09:25 -0500

    Add Jekyll 3 Support

--- spec/pager_spec.rb
+++ spec/pager_spec.rb
@@ -15,7 +15,12 @@ RSpec.describe(Jekyll::Paginate::Pager) do
     let(:site) { build_site }
 
     it "determines the correct pagination path for each page" do
-      expect(described_class.paginate_path(site, 1)).to eql("/index.html")
+      if Jekyll::VERSION < '3.0.0'
+        expect(described_class.paginate_path(site, 1)).to eql("/index.html")
+      else
+        expect(described_class.paginate_path(site, 1)).to eql("/")
+      end
+
       expect(described_class.paginate_path(site, 2)).to eql("/page2")
     end
   end
@@ -24,7 +29,12 @@ RSpec.describe(Jekyll::Paginate::Pager) do
     let(:site) { build_site({'paginate_path' => '/blog/page-:num'}) }
 
     it "determines the correct pagination path for each page" do
-      expect(described_class.paginate_path(site, 1)).to eql("/index.html")
+      if Jekyll::VERSION < '3.0.0'
+        expect(described_class.paginate_path(site, 1)).to eql("/index.html")
+      else
+        expect(described_class.paginate_path(site, 1)).to eql("/")
+      end
+
       expect(described_class.paginate_path(site, 2)).to eql("/blog/page-2")
     end
   end
@@ -33,7 +43,12 @@ RSpec.describe(Jekyll::Paginate::Pager) do
     let(:site) { build_site({'paginate_path' => '/blog/page/:num'}) }
 
     it "determines the correct pagination path for each page" do
-      expect(described_class.paginate_path(site, 1)).to eql("/index.html")
+      if Jekyll::VERSION < '3.0.0'
+        expect(described_class.paginate_path(site, 1)).to eql("/index.html")
+      else
+        expect(described_class.paginate_path(site, 1)).to eql("/")
+      end
+
       expect(described_class.paginate_path(site, 2)).to eql("/blog/page/2")
     end
   end
@@ -42,7 +57,12 @@ RSpec.describe(Jekyll::Paginate::Pager) do
     let(:site) { build_site({'paginate_path' => '/contacts/page:num'}) }
 
     it "determines the correct pagination path for each page" do
-      expect(described_class.paginate_path(site, 1)).to eql("/contacts/index.html")
+      if Jekyll::VERSION < '3.0.0'
+        expect(described_class.paginate_path(site, 1)).to eql("/contacts/index.html")
+      else
+        expect(described_class.paginate_path(site, 1)).to eql("/contacts/")
+      end
+
       expect(described_class.paginate_path(site, 2)).to eql("/contacts/page2")
     end
   end
@@ -51,7 +71,12 @@ RSpec.describe(Jekyll::Paginate::Pager) do
     let(:site) { build_site({'paginate_path' => '/contacts/page/:num'}) }
 
     it "determines the correct pagination path for each page" do
-      expect(described_class.paginate_path(site, 1)).to eql("/contacts/index.html")
+      if Jekyll::VERSION < '3.0.0'
+        expect(described_class.paginate_path(site, 1)).to eql("/contacts/index.html")
+      else
+        expect(described_class.paginate_path(site, 1)).to eql("/contacts/")
+      end
+
       expect(described_class.paginate_path(site, 2)).to eql("/contacts/page/2")
     end
   end
@@ -74,15 +99,23 @@ RSpec.describe(Jekyll::Paginate::Pager) do
 
   context "pagination enabled for 2" do
     let(:site)  { build_site('paginate' => 2) }
-    let(:posts) { site.posts }
+    if Jekyll::VERSION < '3.0.0'
+      let(:posts) { site.posts }
+    else
+      let(:posts) { site.posts.docs }
+    end
 
     it "report that pagination is enabled" do
       expect(described_class.pagination_enabled?(site)).to be_truthy
     end
-
+  
     context "with 4 posts" do
-      let(:posts) { site.posts[1..4] }
-
+      if Jekyll::VERSION < '3.0.0'
+        let(:posts) { site.posts[1..4] }
+      else
+        let(:posts) { site.posts.docs[1..4] }
+      end
+  
       it "create first pager" do
         pager = described_class.new(site, 1, posts)
         expect(pager.posts.size).to eql(2)
@@ -90,7 +123,7 @@ RSpec.describe(Jekyll::Paginate::Pager) do
         expect(pager.previous_page).to be_nil
         expect(pager.next_page).to eql(2)
       end
-
+  
       it "create second pager" do
         pager = described_class.new(site, 2, posts)
         expect(pager.posts.size).to eql(2)
@@ -98,16 +131,19 @@ RSpec.describe(Jekyll::Paginate::Pager) do
         expect(pager.previous_page).to eql(1)
         expect(pager.next_page).to be_nil
       end
-
+  
       it "not create third pager" do
         expect { described_class.new(site, 3, posts) }.to raise_error
       end
-
     end
 
     context "with 5 posts" do
-      let(:posts) { site.posts[1..5] }
-
+      if Jekyll::VERSION < '3.0.0'
+        let(:posts) { site.posts[1..5] }
+      else
+        let(:posts) { site.posts.docs[1..5] }
+      end
+  
       it "create first pager" do
         pager = described_class.new(site, 1, posts)
         expect(pager.posts.size).to eql(2)
@@ -115,7 +151,7 @@ RSpec.describe(Jekyll::Paginate::Pager) do
         expect(pager.previous_page).to be_nil
         expect(pager.next_page).to eql(2)
       end
-
+  
       it "create second pager" do
         pager = described_class.new(site, 2, posts)
         expect(pager.posts.size).to eql(2)
@@ -123,7 +159,7 @@ RSpec.describe(Jekyll::Paginate::Pager) do
         expect(pager.previous_page).to eql(1)
         expect(pager.next_page).to eql(3)
       end
-
+  
       it "create third pager" do
         pager = described_class.new(site, 3, posts)
         expect(pager.posts.size).to eql(1)
@@ -131,7 +167,7 @@ RSpec.describe(Jekyll::Paginate::Pager) do
         expect(pager.previous_page).to eql(2)
         expect(pager.next_page).to be_nil
       end
-
+  
       it "not create fourth pager" do
         expect { described_class.new(site, 4, posts) }.to raise_error(RuntimeError)
       end
