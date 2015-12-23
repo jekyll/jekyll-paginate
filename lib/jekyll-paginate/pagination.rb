b4c66ee1100c006de4d5176e147914f6d3873269
Kevin McCarthy <kjmagic13@gmail.com>
2015-12-23 12:31:59 -0500

    Update pagination.rb

--- lib/jekyll-paginate/pagination.rb
+++ lib/jekyll-paginate/pagination.rb
@@ -38,7 +38,7 @@ module Jekyll
       #                   "previous_page" => <Number>,
       #                   "next_page" => <Number> }}
       def paginate(site, page)
-        all_posts = site.site_payload['site']['posts'].reject { |post| post['hidden'] || !in_category(site, post) }
+        all_posts = site.site_payload['site']['posts'].reject { |post| post['hidden'] || !in_category?(site, post) }
         pages = Pager.calculate_pages(all_posts, site.config['paginate'].to_i)
         (1..pages).each do |num_page|
           pager = Pager.new(site, num_page, all_posts, pages)
